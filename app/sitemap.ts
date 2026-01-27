a04d48e24ec0aca400b653f20d288369d6456285
naiawa <karake.shoya@gmail.com>
2026-01-27 16:24:59 +0900

    fix: sitemapのbaseUrlの修正

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://dream-oracle-app.vercel.app'; // TODO: 実際のドメインに変更
+  const baseUrl = 'https://dream-analysis-app.netlify.app/'; // TODO: 実際のドメインに変更
 
   // 静的ページの定義
   const routes = [
