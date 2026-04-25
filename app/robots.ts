dfd9fb333c22e7e7d4cd1055e229c887eda17714
naiawa <karake.shoya@gmail.com>
2026-04-25 11:49:00 +0900

    fix: ソーシャルクローラー（Facebook等）のOGPクロールをunblockする

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,25 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/private/',
-    },
-    sitemap: 'https://yume-insight.com/sitemap.xml'
+    rules: [
+      {
+        userAgent: 'facebookexternalhit',
+        allow: '/',
+      },
+      {
+        userAgent: 'Twitterbot',
+        allow: '/',
+      },
+      {
+        userAgent: 'LinkedInBot',
+        allow: '/',
+      },
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: '/private/',
+      },
+    ],
+    sitemap: 'https://yume-insight.com/sitemap.xml',
   };
 }
